50bb1043bbee1700b514babecb04efc5ed3eb6c5
Ayush Jain <jain18ayush@gmail.com>
2024-12-10 15:29:42 -0600

    added hypothesis testing

--- structs/utils.py
+++ structs/utils.py
@@ -7,4 +7,10 @@ class LayerConfig:
     def __init__(self, name, input_dim):
         self.name = name
         self.input_dim = input_dim
-
+        
+# Create instances for each layer
+fc1_config = LayerConfig('fc1', 256)
+fc2_config = LayerConfig('fc2', 128)
+fc3_config = LayerConfig('fc3', 10)
+encoder_config = LayerConfig('encoder', 2304)
+decoder_config = LayerConfig('decoder', 128)
